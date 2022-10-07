4517230d61edbdebe419b391e4d41b275824edf0
Ilya Zakhvatkin <iley.zakhvatkin@yandex.ru>
2022-10-07 16:49:58 +0300

    Module_6_v_0.7

---
+++ app/Models/Article.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    public $fillable = ['slug', 'title', 'brief', 'fulltext', 'active'];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
---
+++ database/migrations/2022_10_06_204336_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->string('slug', 100)->unique();
+            $table->string('title', 100);
+            $table->string('brief', 255)->nullable();
+            $table->text('fulltext')->nullable();
+            $table->boolean('active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
