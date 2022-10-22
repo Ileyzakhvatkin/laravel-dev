322c07f95a73fd416b1ba4494ea17bb1cb4b796a
Ilya Zakhvatkin <iley.zakhvatkin@yandex.ru>
2022-10-22 16:12:30 +0300

    Module_10_v_0.2

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -3,7 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
 
 class ArticleFactory extends Factory
 {
@@ -14,13 +14,16 @@ class ArticleFactory extends Factory
      */
     public function definition()
     {
+        $days = rand(10,900);
         return [
             'slug' => 'post-' . $this->faker->unique()->numberBetween(1,100),
-            'title' => $this->faker->words(3, true),
-            'brief' => $this->faker->words(8, true),
-            'fulltext' => $this->faker->words(20, true),
+            'title' => $this->faker->words(rand(3,7), true),
+            'brief' => $this->faker->words(rand(10,20), true),
+            'fulltext' => $this->faker->words(rand(30,50), true),
             'active' => $this->faker->numberBetween(0,1),
             'owner_id' => 1,
+            'created_at' => Carbon::now()->subDays($days),
+            'updated_at' => Carbon::now()->subDays($days - rand(0,9)),
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,14 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Tag::factory(rand(10,15))->create();
+        \App\Models\Tag::factory(rand(8,15))->create();
         \App\Models\User::factory(rand(2,3))->create()
             ->each(function (\App\Models\User $user) {
                 $user->roles()->attach(2);
                 \App\Models\Article::factory(rand(10,12))->create([ 'owner_id' => $user ])
                     ->each(function (\App\Models\Article $article) {
-                        $randomTag = \App\Models\Tag::all()->random(rand(1,5));
-                        $article->tags()->saveMany($randomTag);
+                        $article->tags()->saveMany(\App\Models\Tag::all()->random(rand(2,5)));
                     });
             });
     }
