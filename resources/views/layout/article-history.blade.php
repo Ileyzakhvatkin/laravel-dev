a6a99966de4d164a8660a7471141ad3cc9b824e8
Ilya Zakhvatkin <iley.zakhvatkin@yandex.ru>
2022-10-27 22:32:43 +0300

    Module_12_v_1.0

--- resources/views/layout/article-history.blade.php
+++ resources/views/layout/article-history.blade.php
@@ -1,5 +1,5 @@
 <hr>
-<h4>Изменения статьи</h4>
+<h4>История изменений статьи</h4>
     @forelse($article->history as $item)
         <div class="bg-light p-3 my-3">
             <div class="alert alert-primary">{{ $item->name }} - {{ $item->pivot->created_at->diffForHumans() }}</div>
