dbb383a551cdf4b57508ed89427346dc63054bf2
Ilya Zakhvatkin <iley.zakhvatkin@yandex.ru>
2022-11-07 22:08:54 +0300

    Module_15_v_0.2

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -16,23 +16,18 @@ class ReportController extends Controller
     public function report()
     {
         $allTables = [
-            'news' => 'Новости',
-            'articles' => 'Статьи',
-            'comments'=> 'Комментарии',
-            'tags' => 'Теги',
-            'users' => 'Пользователи',
-        ];
-        $requestTables = [
-            'news' => 'on',
-            'comments'=> 'on',
+            'news' => 'Новостей',
+            'articles' => 'Статей',
+            'comments'=> 'Комментариев',
+            'tags' => 'Тегов',
+            'users' => 'Пользователей',
         ];
         $reportData = [];
         foreach ( $allTables as $key => $table ) {
-            if ( array_key_exists($key, $requestTables) ) {
+            if ( array_key_exists($key, \request()->all() ) ) {
                 $reportData[$table] = \DB::table($key)->count();
             }
         }
-
         return redirect('/admin/report')->with('report', $reportData);
     }
 }
--- resources/views/admin/report.blade.php
+++ resources/views/admin/report.blade.php
@@ -6,34 +6,39 @@
 
             <form class="p-4 mb-3 bg-light rounded" method="POST" action="/admin/report">
                 @csrf
-                <h4 class="blog-post-title mb-3">Данные для отчета</h4>
+                <h4 class="blog-post-title mb-3">Посчитать сколько на сайте</h4>
                 <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="news" name="news" @if(\request()->input('news')) checked="checked" @endif>
-                    <label class="form-check-label" for="news">Новости</label>
+                    <input type="checkbox" class="form-check-input" id="news" name="news"
+                           @if( session('report') && array_key_exists('Новостей', session('report')) ) checked="checked" @endif>
+                    <label class="form-check-label" for="news">Новостей</label>
                 </div>
                 <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="articles" name="articles" @if(\request()->input('articles')) checked="checked" @endif>
-                    <label class="form-check-label" for="articles">Статьи</label>
+                    <input type="checkbox" class="form-check-input" id="articles" name="articles"
+                           @if( session('report') && array_key_exists('Статей', session('report')) ) checked="checked" @endif>
+                    <label class="form-check-label" for="articles">Статей</label>
                 </div>
                 <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="comments" name="comments" @if(\request()->input('comments')) checked="checked" @endif>
-                    <label class="form-check-label" for="comments">Комментарии</label>
+                    <input type="checkbox" class="form-check-input" id="comments" name="comments"
+                           @if( session('report') && array_key_exists('Комментариев', session('report')) ) checked="checked" @endif>
+                    <label class="form-check-label" for="comments">Комментариев</label>
                 </div>
                 <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="tags" name="tags" @if(\request()->input('tags')) checked="checked" @endif>
-                    <label class="form-check-label" for="tags">Теги</label>
+                    <input type="checkbox" class="form-check-input" id="tags" name="tags"
+                           @if( session('report') && array_key_exists('Тегов', session('report')) ) checked="checked" @endif>
+                    <label class="form-check-label" for="tags">Тегов</label>
                 </div>
                 <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="users" name="users" @if(\request()->input('users')) checked="checked" @endif>
-                    <label class="form-check-label" for="users">Пользователи</label>
+                    <input type="checkbox" class="form-check-input" id="users" name="users"
+                           @if( session('report') && array_key_exists('Пользователей', session('report')) ) checked="checked" @endif>
+                    <label class="form-check-label" for="users">Пользователей</label>
                 </div>
-                <button type="submit" class="btn btn-outline-primary">Сформировать</button>
+                <button type="submit" class="btn btn-outline-primary">Сгенерировать отчёт</button>
             </form>
-            @if( isset($reportData) )
+            @if( session('report') )
             <div class="p-4 mb-3 bg-light rounded">
                 <h2 class="blog-post-title mb-3">Отчет</h2>
                 <table class="table table-striped">
-                    @foreach($reportData as $key => $item)
+                    @foreach(session('report') as $key => $item)
                         <tr>
                             <td>{{ $key }}</td>
                             <td>{{ $item }}</td>
